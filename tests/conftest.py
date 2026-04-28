968c07e91c43e97ebcd8e76c7d7f13902ddf7bed
vikdevit <v.khatcherian@outlook.fr>
2026-04-28 19:30:18 +0200

    prepa sans mode sans db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,13 @@
 import pytest
+import os
 from app.db.database import get_connection
 
 @pytest.fixture
 def db_conn():
+    # Si on est en CI → on skip
+    #if os.getenv("CI") == "true":
+    #    pytest.skip("Skipping DB tests in CI")
+
     conn = get_connection()
     yield conn
     conn.close()
